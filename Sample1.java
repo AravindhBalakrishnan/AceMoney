d507b5caff39206c4334c9d829b130c119b6e4c1
Aravindh <76097872+AravindhBalakrishnan@users.noreply.github.com>
2023-11-15 12:07:22 +0530

    Update Sample1.java
    adding m1 method

--- Sample1.java
+++ Sample1.java
@@ -1,12 +1,23 @@
 package Task;
 
 public class Main {
-	
-		  public static void main(String[] args) {
-		    int x = 20;
-		    int y = 18;
-		    if (x > y) {
+
+	public void m1()
+	{
+
+		int x = 20;
+		int y = 18;
+		if (x > y) {
 		      System.out.println("x is greater than y");
+
+
+	}
+	
+		  public static void main(String[] args) 
+		  {
+		     Main m=new Main();
+
+		    m.m1();
 		    }  
 		  }
 		}
