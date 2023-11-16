25492e18c59eaf78b421beba9ddeb0415fd3067a
Aravindh <76097872+AravindhBalakrishnan@users.noreply.github.com>
2023-11-16 11:42:04 +0530

    Update add function Sample1.java

--- Sample1.java
+++ Sample1.java
@@ -1,22 +1,25 @@
 package Task;
 
-public class Main {
+public class Main 
+{
 
 	public void m1(int x, int y)
 	{
-
-		
 		if (x > y) {
 		      System.out.println("x is greater than y");
 
-
 	}
-	
-		  public static void main(String[] args) 
-		  {
-		     Main m=new Main();
 
-		    m.m1(20,18);
-		    }  
-		  }
-		}
+	public void add()
+	{
+		System.out.println("add method");
+	}
+		
+	public static void main(String[] args) 
+	{
+		Main m=new Main();
+
+		m.m1(20,18);
+	}  
+   }
+}
